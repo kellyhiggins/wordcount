2a6724591aa509359b3505ede5458b9307649a5c
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 13:02:27 -0700

    prints dictionary, but with wrong counts

---
+++ wordcount.py
@@ -0,0 +1,29 @@
+# put your code here.
+
+def count_words(file_name):
+    """Counts how many times each space-separated word appears in text file """
+
+    word_count = {}
+    data_file = open(file_name)
+
+    word_frequency = 0
+
+    for line in data_file:
+        word_list = line.split(' ')
+        for word in word_list:
+            if word not in word_count:
+                word_count[word] = word_frequency
+                word_frequency += 1
+            else:
+                word_frequency += 1
+
+    print word_count
+    return
+
+count_words("test.txt")
+
+# our function would find the value of word and frequency,
+# then create a dictionary by taking those arguments
+# def dictionary(key, value):
+#   dictionary = {key: value}
+
